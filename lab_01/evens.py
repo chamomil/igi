e2a5c3a3fe884db57273f88a2bb5cd42bcf00d8f
Alyona <alyonka.m.by@gmail.com>
2023-03-03 19:14:16 +0300

    update evens func

--- lab_01/evens.py
+++ lab_01/evens.py
@@ -1,15 +1,2 @@
-import random
-
-
-def evens():
-    list_of_nums = []
-    list_of_even = []
-
-    for i in range(10):
-        num = random.randint(1, 100)
-        list_of_nums.append(num)
-
-        if num % 2 == 0:
-            list_of_even.append(num)
-
-    return list_of_even
+def evens(list_of_nums):
+    return [num for num in list_of_nums if num % 2 == 0]
