b47f209b2492aeb82ba0773739113a1ae9f41c51
Alyona <alyonka.m.by@gmail.com>
2023-05-13 20:21:48 +0300

    add serialization of collections, fixed primitives

--- lab_03/json_serializer.py
+++ lab_03/json_serializer.py
@@ -1,20 +1,58 @@
+import types
+
+
 class JsonSerializer:
-    @staticmethod
-    def dumps(obj):
+
+    def dumps(self, obj):
         if isinstance(obj, bool):
             return str(obj).lower()
         elif isinstance(obj, int | float):
             return str(obj)
         elif isinstance(obj, str):
-            return '"' + obj + '"'
+            return f'"{obj}"'
+        elif isinstance(obj, list):
+            result = ", ".join([self.dumps(i) for i in obj])
+            return f"[{result}]"
+        elif isinstance(obj, tuple):
+            return '{"tuple": ' + f"{self.dumps(list(obj))}" + "}"
+        elif isinstance(obj, set):
+            return '{"set": ' + f"{self.dumps(list(obj))}" + "}"
+        elif isinstance(obj, frozenset):
+            return '{"frozenset": ' + f"{self.dumps(list(obj))}" + "}"
+        elif isinstance(obj, dict):
+            result = ", ".join([f"{self.dumps(i)}: {self.dumps(obj[i])}" for i in obj.keys()])
+            return "{" + result + "}"
+        elif isinstance(obj, types.FunctionType):
+            return '{"function": ' + f'["{obj.__closure__}", "{obj.__code__}", "{obj.__defaults__}", ' \
+                                     f'"{obj.__dict__}", "{obj.__doc__}", "{obj.__globals__}", "{obj.__name__}"]' + "}"
         else:
-            return str(obj)
+            return "null"
 
-    @staticmethod
-    def loads(string: str):
+    def loads(self, string: str):
         if string.isnumeric():
             return float(string)
-        elif string[0] == '"':
-            return string.replace('"', '')
-        else:
-            return bool(string)
+        elif string[0] == '"' or string[0] == "'":
+            return string.strip('"\'')
+        elif string == "true" or string == "false":
+            return string == "true"
+        elif string == "null":
+            return
+        elif string[0] == '[':  # for list
+            elements = string[1:-1].split(", ")
+            return [self.loads(i) for i in elements]
+        elif string[0] == '{':  # for dict
+            return self._get_dict(string[1:-1].split(": "))
+
+    def _get_dict(self, string):
+        try:
+            if string[0] == '"set"':
+                return set(self.loads(string[1]))
+            elif string[0] == '"tuple"':
+                return tuple(self.loads(string[1]))
+            elif string[0] == '"frozenset"':
+                return frozenset(self.loads(string[1]))
+            elif string[0] == '"function"':
+                return
+        except TypeError:
+            return {self.loads(string[0]): self.loads(string[1])}
+        return {self.loads(string[0]): self.loads(string[1])}
--- lab_03/main.py
+++ lab_03/main.py
@@ -1,19 +1,26 @@
 import json
+import types
+
 from serializer import Serializer
+
+
+def func():
+    print("hello")
 
 
 def main():
     json_ser = Serializer().get_serializer("json")
-    val = "abc"
+    val = func
+    k = val.__code__
+    print(type(k))
     b = json_ser.dumps(val)
     print(b)
-    d = json_ser.loads(b)
-    print(d)
-    a = json.dumps(val)
-    print(a)
-    c = json.loads(a)
-    print(c)
-
+    # d = json_ser.loads(b)
+    # print(d)
+    # a = json.dumps(val)
+    # print(a)
+    # c = json.loads(a)
+    # print(c)
 
 
 if __name__ == '__main__':
