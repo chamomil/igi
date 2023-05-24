de53e355637b7ceb2ecc70c6f32a9ffad50470dd
Alyona <alyonka.m.by@gmail.com>
2023-05-24 12:59:38 +0300

    change setup

--- lab_03/setup.py
+++ lab_03/setup.py
@@ -4,10 +4,13 @@ setup(
     name='makarenko-serializer',
     version='0.1.0',
     packages=['makarenko_serializer'],
+    license='MIT',
+    description='json/xml serializer',
     entry_points={
         "console_scripts": [
             "makarenko_serializer = makarenko_serializer.makarenko_serializer:main"
         ]
     },
     author='alyona makarenko',
+    author_email='alyonka.m.by@gmail.com',
 )
