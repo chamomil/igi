02cd63d21c2cfe73ac8b3d102a883c3bab188c64
Alyona <alyonka.m.by@gmail.com>
2023-06-03 13:21:42 +0300

    change views

--- lab_04/cinema/catalog/views.py
+++ lab_04/cinema/catalog/views.py
@@ -1,5 +1,17 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+
+from .models import Movie
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class IndexView(generic.ListView):
+    template_name = "catalog/index.html"
+    context_object_name = "movies_list"
+
+    def get_queryset(self):
+        return Movie.objects.order_by("-year_of_production")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Movie
+    template_name = "catalog/detail.html"
