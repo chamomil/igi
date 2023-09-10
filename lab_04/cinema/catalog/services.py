81261a4fed9cf790967235687627debf8fbeef77
Alyona <alyonka.m.by@gmail.com>
2023-09-10 08:23:15 +0300

    disable api

--- lab_04/cinema/catalog/services.py
+++ lab_04/cinema/catalog/services.py
@@ -2,15 +2,16 @@ import requests
 
 
 def get_random_dog():
-    try:
-        random_dog = requests.get("https://random.dog/woof.json").json()
-        while random_dog.get('url').endswith('.mp4'):
-            random_dog = requests.get("https://random.dog/woof.json").json()
-        return random_dog.get("url")
-    except:
-        return ""
+    # try:
+    #     random_dog = requests.get("https://random.dog/woof.json").json()
+    #     while random_dog.get('url').endswith('.mp4'):
+    #         random_dog = requests.get("https://random.dog/woof.json").json()
+    #     return random_dog.get("url")
+    # except:
+    return ""
 
 
 def get_random_joke():
-    response = requests.get("https://icanhazdadjoke.com", headers={'Accept': 'application/json'}).json()
-    return response.get('joke')
+    return
+    # response = requests.get("https://icanhazdadjoke.com", headers={'Accept': 'application/json'}).json()
+    # return response.get('joke')
